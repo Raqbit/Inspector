a7b8cbf54a9c2f61c1975d29ac926b5c75d25682
Vauff <mctehkitti@gmail.com>
2017-08-12 17:52:00 -0400

    Bump version to 0.6.5

--- src/main/java/io/github/hsyyid/inspector/PluginInfo.java
+++ src/main/java/io/github/hsyyid/inspector/PluginInfo.java
@@ -7,6 +7,6 @@ public abstract class PluginInfo
 {
 	public static final String ID = "inspector";
 	public static final String NAME = "Inspector";
-	public static final String VERSION = "0.6.4";
+	public static final String VERSION = "0.6.5";
 	public static final String DESCRIPTION = "This plugin enables servers to monitor griefing and provides rollback.";
 }
